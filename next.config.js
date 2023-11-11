949cb088b22fb749f27002c56ee7072c2bfb0e9a
StealthWorm <thierrypitela@hotmail.com>
2023-11-11 11:59:43 -0300

    feat: creation and configuration of next project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  //* definimos quais são as extensões que queremos que gerem paginas
+  pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+}
 
 module.exports = nextConfig
