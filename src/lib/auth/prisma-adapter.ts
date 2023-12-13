c21ba2376f14b93a5142369481f8157e14a230f4
StealthWorm <thierrypitela@hotmail.com>
2023-12-12 22:52:00 -0300

    chore: created adapter methods

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -37,26 +37,138 @@ export function PrismaAdapter(): Adapter {
         avatar_url: user.avatar_url!,
       }
     },
-    async getUserByAccount({ providerAccountId, provider }) {},
+    async getUserByAccount({ providerAccountId, provider }) {
+      const { user } = await prisma.account.findUniqueOrThrow({
+        where: {
+          provider_provider_account_id: {
+            provider,
+            provider_account_id: providerAccountId,
+          },
+        },
+        include: {
+          user: true,
+        },
+      })
 
-    async updateUser(user) {},
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      }
+    },
 
-    async deleteUser(userId) {},
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
 
-    async linkAccount(account) {},
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      }
+    },
 
-    async unlinkAccount({ providerAccountId, provider }) {},
+    // async deleteUser(userId) {},
 
-    async createSession({ sessionToken, userId, expires }) {},
+    async linkAccount(account) {
+      await prisma.account.create({
+        data: {
+          user_id: account.userId,
+          type: account.type,
+          provider: account.provider,
+          provider_account_id: account.providerAccountId,
+          refresh_token: account.refresh_token,
+          access_token: account.access_token,
+          expires_at: account.expires_at,
+          token_type: account.token_type,
+          scope: account.scope,
+          id_token: account.id_token,
+          session_state: account.session_state,
+        },
+      })
+    },
 
-    async getSessionAndUser(sessionToken) {},
+    // async unlinkAccount({ providerAccountId, provider }) {},
 
-    async updateSession({ sessionToken }) {},
+    async createSession({ sessionToken, userId, expires }) {
+      await prisma.session.create({
+        data: {
+          user_id: userId,
+          expires,
+          session_token: sessionToken,
+        },
+      })
 
-    async deleteSession(sessionToken) {},
+      return {
+        userId,
+        sessionToken,
+        expires,
+      }
+    },
 
-    async createVerificationToken({ identifier, expires, token }) {},
+    async getSessionAndUser(sessionToken) {
+      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+        where: {
+          session_token: sessionToken,
+        },
+        include: {
+          user: true,
+        },
+      })
 
-    async useVerificationToken({ identifier, token }) {},
+      return {
+        session: {
+          userId: session.user_id,
+          expires: session.expires,
+          sessionToken: session.session_token,
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          username: user.username,
+          email: user.email!,
+          emailVerified: null,
+          avatar_url: user.avatar_url!,
+        },
+      }
+    },
+
+    async updateSession({ sessionToken, userId, expires }) {
+      const prismaSession = await prisma.session.update({
+        where: {
+          session_token: sessionToken,
+        },
+        data: {
+          expires,
+          user_id: userId,
+        },
+      })
+
+      return {
+        sessionToken: prismaSession.session_token,
+        userId: prismaSession.user_id,
+        expires: prismaSession.expires,
+      }
+    },
+
+    // async deleteSession(sessionToken) {},
+
+    // async createVerificationToken({ identifier, expires, token }) {},
+
+    // async useVerificationToken({ identifier, token }) {},
   }
 }
