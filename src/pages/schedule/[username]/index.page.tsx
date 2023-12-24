6e3b8b8e92f51110b030c22a0ba8e9ec28038073
StealthWorm <thierrypitela@hotmail.com>
2023-12-24 09:27:34 -0300

    feat: config callendar

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,63 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { Container, UserHeader } from './styles'
+import { ScheduleForm } from './ScheduleForm'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+
+      <ScheduleForm />
+    </Container>
+  )
+}
+
+// getStaticPaths exige a criação de paths para carregar na tela no momento da build
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+//  como são carregados em momento de build, não possuem parametros req,res
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
